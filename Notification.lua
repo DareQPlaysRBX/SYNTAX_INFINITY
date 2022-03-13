7c63c3ff0bde770168422896a7d903454bbef531
DareQPlaysDeV <80836947+DareQPlaysRBX@users.noreply.github.com>
2022-03-13 16:24:07 +0100

    Update Notification.lua

--- Notification.lua
+++ Notification.lua
@@ -2,7 +2,7 @@ getgenv().IrisAd = true
 local Notification = loadstring(game:HttpGet("https://api.irisapp.ca/Scripts/IrisBetterNotifications.lua"))()
 
 Notification.Notify("[ SYNTAX-INFINITY ]:", "Attached successfully!", "rbxassetid://8620551423", {
-    Duration = 5,
+    Duration = 7,
     
     TitleSettings = {
         TextXAlignment = Enum.TextXAlignment.Center,
