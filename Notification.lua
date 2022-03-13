bc2b9777a2109f3ee09eb29b79db151cc278541a
DareQPlaysDeV <80836947+DareQPlaysRBX@users.noreply.github.com>
2022-03-13 20:11:21 +0100

    Update Notification.lua

--- Notification.lua
+++ Notification.lua
@@ -1,1 +1,18 @@
+getgenv().IrisAd = true
+local Notification = loadstring(game:HttpGet("https://api.irisapp.ca/Scripts/IrisBetterNotifications.lua"))()
 
+Notification.Notify("[ SYNTAX-INFINITY ]:", "Połączono pomyślnie!", "rbxassetid://9091834179", {
+    Duration = 7,
+    
+    TitleSettings = {
+        TextXAlignment = Enum.TextXAlignment.Center,
+        Font = Enum.Font.SourceSansSemibold,
+    },
+
+    GradientSettings = {
+        GradientEnabled = true,
+        SolidColorEnabled = false,
+        SolidColor = Color3.fromRGB(150, 83, 240),
+        Retract = true
+    }
+})
