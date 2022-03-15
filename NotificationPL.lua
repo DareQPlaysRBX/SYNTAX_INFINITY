cdee38d526a61ee7f87e037ca4f2dd7409e99341
DareQPlaysDeV <80836947+DareQPlaysRBX@users.noreply.github.com>
2022-03-15 15:35:53 +0100

    Update NotificationPL.lua

--- NotificationPL.lua
+++ NotificationPL.lua
@@ -1,7 +1,7 @@
 getgenv().IrisAd = true
 local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/DareQPlaysRBX/SYNTAX_INFINITY/main/IrisSource.lua"))()
 
-Notification.Notify("[ SYNTAX-INFINITY ]:", "Połączono pomyślnie!", "rbxassetid://9091857649", {
+Notification.Notify("[ SYNTAX-INFINITY ]:", "Połączono pomyślnie!", "rbxassetid://9108619266", {
     Duration = 7,
     
     TitleSettings = {
@@ -14,5 +14,10 @@ Notification.Notify("[ SYNTAX-INFINITY ]:", "Połączono pomyślnie!", "rbxasset
         SolidColorEnabled = false,
         SolidColor = Color3.fromRGB(150, 83, 240),
         Retract = true
-    }
+    },
+    
+    IconSettings = {
+		BackgroundTransparency = 1,
+		BackgroundColor3 = Color3.fromRGB(255, 255, 255),				
+   }
 })
