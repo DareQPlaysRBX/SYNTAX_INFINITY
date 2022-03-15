722a40387ee8369722dc5fbe7da8babe540c2730
DareQPlaysDeV <80836947+DareQPlaysRBX@users.noreply.github.com>
2022-03-15 16:33:04 +0100

    Update NotificationENG.lua

--- NotificationENG.lua
+++ NotificationENG.lua
@@ -1,7 +1,7 @@
 getgenv().IrisAd = true
 local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/DareQPlaysRBX/SYNTAX_INFINITY/main/IrisSource.lua"))()
 
-Notification.Notify("[ SYNTAX-INFINITY ]:", "Attached successfully!", "rbxassetid://9108619266", {
+Notification.Notify("[ SYNTAX-INFINITY ]:", "Attached successfully!", "rbxassetid://9108997737", {
     Duration = 7,
     
     TitleSettings = {
