3c668a40bf0ac6bc7f52e4daa5a346d73688769b
DareQPlaysDeV <80836947+DareQPlaysRBX@users.noreply.github.com>
2022-04-03 16:01:18 +0200

    Update StatusHost.lua

--- StatusHost.lua
+++ StatusHost.lua
@@ -1,1 +1,23 @@
-status=working
+getgenv().IrisAd = true
+local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/DareQPlaysRBX/SYNTAX_INFINITY/main/IrisSource.lua"))()
+
+Notification.Notify("[ SYNTAX-INFINITY ]:", "Already Attached!", "rbxassetid://9108997737", {
+    Duration = 7,
+    
+    TitleSettings = {
+        TextXAlignment = Enum.TextXAlignment.Center,
+        Font = Enum.Font.SourceSansSemibold,
+    },
+
+    GradientSettings = {
+        GradientEnabled = true,
+        SolidColorEnabled = false,
+        SolidColor = Color3.fromRGB(150, 83, 240),
+        Retract = true
+    },
+    
+    IconSettings = {
+	BackgroundTransparency = 1,
+	BackgroundColor3 = Color3.fromRGB(255, 255, 255),				
+   }
+})
