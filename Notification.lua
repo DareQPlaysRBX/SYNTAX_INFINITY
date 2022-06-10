3e311dba1db8c7f7b6e34c83bd62cbc76d372489
DareQPlaysDeV <80836947+DareQPlaysRBX@users.noreply.github.com>
2022-06-10 14:53:33 +0200

    Delete Notification.lua

--- Notification.lua
+++
@@ -1,9 +0,0 @@
-getgenv().IrisAd = true
-local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/DareQPlaysRBX/SYNTAX_INFINITY/main/IrisSource.lua"))()
-
-Notification.Notify("[SYNTAX-INFINITY]", "Połączono Pomyślnie", "rbxassetid://9108997737", {
-    Duration = 5,       
-    Main = {
-        Rounding = true,
-    }
-});
